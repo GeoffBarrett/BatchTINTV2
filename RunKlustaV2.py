270e06b0766da33612512bd3dcd4854cbbbcb406
GeoffBarrett <Geoffrey.M.Barrett@gmail.com>
2016-06-20 15:54:28 -0400

    changed the inactive tetrode functionality

--- RunKlustaV2.py
+++ RunKlustaV2.py
@@ -310,40 +310,38 @@ class runKlusta():
                                         not_active = ': Tetrode ' + str(tetrode) + ' is not active within the ' + \
                                                      str(set_file[:-1]) + ' set file!'
                                         print('[' + str(cur_date) + ' ' + str(cur_time)[:8] + ']' + not_active)
-
-                                        x = 1
-                                        while x == 1:
-                                            try:
-                                                try:
-                                                    # moves the log files
-                                                    try:
-                                                        os.rename(log_fpath,
-                                                                  os.path.join(log_f_dir, tet_fname + '_log.txt'))
-                                                    except FileNotFoundError:
-                                                        pass
-
-                                                except FileExistsError:
-                                                    os.remove(os.path.join(log_f_dir, tet_fname + '_log.txt'))
-                                                    os.rename(log_fpath,
-                                                              os.path.join(log_f_dir, tet_fname + '_log.txt'))
-                                                try:
-                                                    # moves the .ini files
-                                                    os.rename(ini_fpath, os.path.join(ini_f_dir, tet_fname + '.ini'))
-                                                except FileExistsError:
-                                                    os.remove(os.path.join(ini_f_dir, tet_fname + '.ini'))
-                                                    os.rename(ini_fpath, os.path.join(ini_f_dir, tet_fname + '.ini'))
-
-                                                os.rename(tet_path, os.path.join(inactive_tet_dir, tet_fname))
-
-                                                x = 0
-
-                                            except PermissionError:
-                                                x = 1
-                                        processing = 0
                                         break
 
-                                    else:
+                        x = 1
+                        while x == 1:
+                            try:
+                                try:
+                                    # moves the log files
+                                    try:
+                                        os.rename(log_fpath,
+                                                  os.path.join(log_f_dir, tet_fname + '_log.txt'))
+                                    except FileNotFoundError:
                                         pass
+
+                                except FileExistsError:
+                                    os.remove(os.path.join(log_f_dir, tet_fname + '_log.txt'))
+                                    os.rename(log_fpath,
+                                              os.path.join(log_f_dir, tet_fname + '_log.txt'))
+                                try:
+                                    # moves the .ini files
+                                    os.rename(ini_fpath, os.path.join(ini_f_dir, tet_fname + '.ini'))
+                                except FileExistsError:
+                                    os.remove(os.path.join(ini_f_dir, tet_fname + '.ini'))
+                                    os.rename(ini_fpath, os.path.join(ini_f_dir, tet_fname + '.ini'))
+
+                                os.rename(tet_path, os.path.join(inactive_tet_dir, tet_fname))
+
+                                x = 0
+
+                            except PermissionError:
+                                x = 1
+                            processing = 0
+
 
                 try:
                     q.task_done()
